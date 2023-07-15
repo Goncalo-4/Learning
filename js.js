92094a825f406dd8beb1680c869b5423aa51055d
Goncalo-4 <139626442+Goncalo-4@users.noreply.github.com>
2023-07-15 12:29:54 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,209 @@
+//Determinar Variáveis
+// 1 variável pode ter mais que  parametro
+
+var score = 0;
+var gscore = 0;
+var ghost = false;
+var countblink = 10;
+var tag = "Gonxas4";
+var player = {
+    x: 50,
+    y: 100,
+    pacmouth: 320,
+    pacdir: 0,
+    speed: 5,
+};
+
+var enemy = {
+    x: 150,
+    y: 200,
+    speed: 5,
+    moving: 0,
+    dirx: 0,
+    diry: 0,
+    flash: 0,
+    ghosteat: false,
+};
+
+var powerdot = {
+    x: 10,
+    y: 10,
+    powerup: false,
+    pcountdown: 0,
+    ghostNum: 0
+};
+
+//criar a area de jogo, o objecto principal
+
+var canvas = document.createElement("canvas");
+var context = canvas.getContext("2d");
+canvas.height = 400;
+canvas.width = 600;
+
+// Upload da imagem 
+
+mainImage = new Image();
+mainImage.ready = false;
+mainImage.onload = checkReady;
+mainImage.src = "pac.png";
+
+//determinação das ações de cada tecla
+
+var keyclick = {};
+document.addEventListener("keydown", function (event) {
+    keyclick[event.keyCode] = true;
+    move(keyclick);
+}, false);
+
+document.addEventListener("keydown", function (event) {
+    delete keyclick[event.keyCode];
+}, false);
+
+//determinação do que cada tecla faz
+// abrir e fechar a boca
+// evitar sair dos limites da canvas
+
+function move(keyclick) {
+    if (37 in keyclick) { player.x -= player.speed; player.pacdir = 64 };
+    if (38 in keyclick) { player.y -= player.speed; player.pacdir = 96 };
+    if (39 in keyclick) { player.x += player.speed; player.pacdir = 0 };
+    if (40 in keyclick) { player.y += player.speed; player.pacdir = 32 };
+
+    if (player.x >= (canvas.width - 32)) { player.x = 0 };
+    if (player.y >= (canvas.height - 32)) { player.y = 0 };
+    if (player.x < 0) { player.x = (canvas.width - 32) };
+    if (player.y < 0) { player.y = (canvas.height - 32) };
+
+    if (player.pacmouth == 320) { player.pacmouth = 352; } else[player.pacmouth = 320]
+
+    render();
+}
+
+
+
+function checkReady() {
+    this.ready = true;
+    playgame();
+}
+
+// repetição da função playgame e da função render
+
+function playgame() {
+    render();
+    requestAnimationFrame(playgame);
+}
+
+//função para número aleatório 
+
+function myNum(n) {
+    return Math.floor(Math.random() * n);
+}
+
+//Função que renderiza o Jogo
+
+function render() {
+
+    //definição do fundo e limites do fundo
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+
+
+    if (!powerdot.powerup && powerdot.pcountdown < 5) {
+        powerdot.x = myNum(420) + 30;
+        powerdot.y = myNum(250);
+        powerdot.powerup = true;
+    }
+
+    if (!ghost) {
+        enemy.ghostNum = myNum(5) * 64;
+        enemy.x = myNum(450);
+        enemy.y = myNum(250) + 30;
+        ghost = true;
+    }
+
+    if (enemy.moving < 0) {
+        enemy.moving = (myNum(20) * 3) + 10 + myNum(2);
+        enemy.speed = myNum(3) + 1;
+        enemy.dirx = 0;
+        enemy.diry = 0;
+        if (powerdot.ghosteat) { enemy.speed * -1; };
+        if (enemy.moving % 2) {
+            if (player.x < enemy.x) { enemy.dirx = -enemy.speed; } else { enemy.dirx = enemy.speed; }
+        } else {
+            if (player.y < enemy.y) { enemy.diry = -enemy.speed; } else { enemy.diry = enemy.speed; }
+        }
+    }
+
+    enemy.moving--;
+    enemy.x = enemy.x + enemy.dirx;
+    enemy.y = enemy.y + enemy.diry;
+
+    if (enemy.x >= (canvas.width - 32)) { enemy.x = 0 };
+    if (enemy.y >= (canvas.height - 32)) { enemy.y = 0 };
+    if (enemy.x < 0) { enemy.x = (canvas.width - 32) };
+    if (enemy.y < 0) { enemy.y = (canvas.height - 32) };
+
+    // deteção de colisão Ghost
+
+    if (player.x <= (enemy.x + 26) && enemy.x <= (player.x + 26) && player.y <= (enemy.y + 26) && enemy.y <= (player.y + 32)) {
+        console.log('ghost');
+        if (powerdot.ghosteat) { score++; } else { gscore++; }
+
+        player.x = 10;
+        player.y = 100;
+
+        enemy.x = 300;
+        enemy.y = 200;
+
+        powerdot.pcountdown = 0;
+    }
+
+
+
+    // deteção de colisão Power Up
+
+    if (player.x <= powerdot.x && powerdot.x <= (player.x + 32) && player.y <= powerdot.y && powerdot.y <= (player.y + 32)) {
+        console.log('hit');
+        powerdot.powerup = false;
+        powerdot.pcountdown = 500;
+        powerdot.ghostNum = enemy.ghostNum;
+        enemy.ghostNum = 384;
+        powerdot.x = 0;
+        powerdot.y = 0;
+        powerdot.ghosteat = true;
+    }
+
+    if (powerdot.ghosteat) {
+        powerdot.pcountdown--;
+        if (powerdot.pcountdown <= 0) {
+            powerdot.ghosteat = false;
+            enemy.ghostNum = powerdot.ghostNum;
+
+        }
+    }
+    if (powerdot.powerup) {
+        context.fillStyle = "#ffff";
+        context.beginPath();
+        context.arc(powerdot.x, powerdot.y, 10, 0, Math.PI * 2, true);
+        context.fill();
+
+    }
+
+    // enemy Blinking
+
+    if(countblink > 0){countblink--}else{countblink = 5;
+    
+    if (enemy.flash == 0) { enemy.flash = 32; } else { enemy.flash = 0; };}; 
+
+    context.font = "20px Verdana";
+    context.fillStyle = "white"
+    context.fillText("Pacman: " + score + " vs Ghost:" + gscore + "                  " + tag, 2, 18);
+
+    context.drawImage(mainImage, enemy.ghostNum, enemy.flash, 32, 32, enemy.x, enemy.y, 32, 32);
+    context.drawImage(mainImage, player.pacmouth, player.pacdir, 32, 32, player.x, player.y, 32, 32);
+
+}
+
+document.body.appendChild(canvas);
+context.fillText("hellooooworld", 10, 150);
